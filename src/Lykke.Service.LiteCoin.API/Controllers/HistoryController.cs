4bfd079c963e6c26fc251ff9cd5abd7900ecaac8
some-dev <some-dev@gmail.com>
2018-05-14 22:09:08 +0300

    update  capabilities - set dummy observe history functions

---
+++ src/Lykke.Service.LiteCoin.API/Controllers/HistoryController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.Common.Api.Contract.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Service.LiteCoin.API.Controllers
+{
+    public class HistoryController:Controller
+    {
+        [HttpPost("history/from/{address}/observation")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult ObserveFrom(
+            [FromRoute]string address)
+        {
+            return Ok();
+        }
+
+        [HttpPost("history/from/{address}/observation")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult ObserveTo(
+            [FromRoute]string address)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("history/from/{address}/observation")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult DeleteObservationFrom(
+            [FromRoute]string address)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("history/to/{address}/observation")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult DeleteObservationTo(
+            [FromRoute]string address)
+        {
+            return Ok();
+        }
+    }
+}
