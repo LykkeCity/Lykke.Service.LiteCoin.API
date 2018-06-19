e9b60d473c3bb5a17d15c25226f58a4b8dbd52ca
some-dev <some-dev@gmail.com>
2018-06-19 23:28:01 +0300

    use TimerTrigger over aggregation instead of TimerPeriod over inheritance

---
+++ src/Lykke.Job.LiteCoin/Settings/PerodicalHandlerSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Job.LiteCoin.Settings
+{
+    public class PerodicalHandlerSettings
+    {
+        public TimeSpan UpdateBalancesPeriod { get; set; }
+
+        public TimeSpan UpdateFeeRatePeriod { get; set; }
+
+        public TimeSpan UpdateObservableOperationsPeriod { get; set; }
+    }
+}
