8f75b2cf77482fdce5cde11f8116a5e95cb6de00
some-dev <some-dev@gmail.com>
2018-06-19 12:36:11 +0300

    fixes from remarks pr (refactoring)

--- src/Lykke.Service.LiteCoin.API/Extensions/ModelStateExtensions.cs
+++ src/Lykke.Service.LiteCoin.API/Extensions/ModelStateExtensions.cs
@@ -45,7 +45,7 @@ namespace Lykke.Service.LiteCoin.API.Extensions
             }
         }
 
-        public static bool ValidateContinuationToken(this ModelStateDictionary self, string continuation)
+        public static bool IsValidContinuationToken(this ModelStateDictionary self, string continuation)
         {
             if (!string.IsNullOrEmpty(continuation))
             {
@@ -59,6 +59,19 @@ namespace Lykke.Service.LiteCoin.API.Extensions
 
                     return false;
                 }
+            }
+
+            return true;
+        }
+
+
+        public static bool IsValidTakeParameter(this ModelStateDictionary self, int take)
+        {
+            if (take <= 0)
+            {
+                self.AddModelError(nameof(take), "Must be greater than zero");
+
+                return false;
             }
 
             return true;
